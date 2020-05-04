166c5916aef4145fac752619511dfbe5d5951b4f
bappee <robiullislambappy@gmail.com>
2020-05-04 18:59:01 +0600

    result calculator done

---
+++ conditional.js
@@ -0,0 +1,66 @@
+
+// //1st conditional
+
+
+// var biscuit = 10;
+
+// if(biscuit !=10){
+//     console.log('I will eat Biscuit')
+// }
+// else{
+// console.log('i dint eat biscuit')
+// }
+
+// //2nd conditional
+
+// var job =true;
+// var amount=20000;
+// var tall = true;
+
+
+// if(job==false && amount<10000 && tall== true){
+//     console.log('sabbash beta');
+
+// }
+
+// else if(job==false || tall == true ){
+//     console.log('wait kor')
+// }
+// else{
+//     console.log('ja beta vag');
+// }
+
+//3rd conditional
+
+var gotNumber= 56;
+
+if( gotNumber<33 && gotNumber>=0){
+    console.log("You got F grade")
+}
+
+else if( gotNumber>=33 && gotNumber<40){
+    console.log("You got D grade")
+}
+else if( gotNumber>=40 && gotNumber<50){
+    console.log("You got C grade")
+}
+
+else if( gotNumber>=50 && gotNumber<60){
+    console.log("You got B grade")
+}
+
+else if( gotNumber>=60 && gotNumber<70){
+    console.log("You got A- grade")
+}
+
+else if( gotNumber>70 && gotNumber<80){
+    console.log("You got A grade")
+}
+
+else if( gotNumber>80 && gotNumber >=100){
+    console.log("You got A+ grade")
+}
+
+else{
+    console.log('Invalid Number You Input')
+}
---
+++ hello.js
@@ -0,0 +1,5 @@
+ var roll =50;
+ var name= "rahimUddin"
+ var maraKha = true;
+ 
+ console.log(roll,name, maraKha);
